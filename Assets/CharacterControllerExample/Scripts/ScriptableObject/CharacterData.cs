4e6fed957d42a342ea2d52ff223d991e82a0a30e
Yunus SARICA <yunusssarica@gmail.com>
2021-01-09 11:49:46 +0300

    +setup data for all characters

--- Assets/CharacterControllerExample/Scripts/ScriptableObject/CharacterData.cs
+++ Assets/CharacterControllerExample/Scripts/ScriptableObject/CharacterData.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class MovementData
+{
+    public float MaxSpeed;
+    public float MovementForce;
+    public float JumpHeight;
+}
+
+[System.Serializable]
+public class HealthData
+{
+    public float MaxHealth;
+    public float InitialDamage;
+}
+[CreateAssetMenu(fileName = "Character Data", menuName = "Character Data Example", order = 1)]
 public class CharacterData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public MovementData MovementData = new MovementData();
+    public HealthData HealthData = new HealthData();
 }
